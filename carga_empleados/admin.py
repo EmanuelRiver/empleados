bc689ce37ee62fffdb28329a1d47188d2b183c2e
alee1rivero <alee123rivero@gmail.com>
2023-02-22 19:03:41 -0300

    hasta aca llegue el 22/2

--- carga_empleados/admin.py
+++ carga_empleados/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Empleado
+from .models import *
+
 # Register your models here.
 
 admin.site.register(Empleado)
+admin.site.register(empresa_terciarizada)
--- carga_empleados/urls.py
+++ carga_empleados/urls.py
@@ -3,7 +3,7 @@ from carga_empleados import views
 
 urlpatterns = [
     path('', views.Inicio, name="inicio"),
-    path('empleados', views.empleados, name="empleados"),
+    path('empleados', views.empleados),
     path('empresa_terciarizada', views.empresa_terciarizada),
     
 ]
