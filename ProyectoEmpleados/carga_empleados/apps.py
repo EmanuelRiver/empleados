82df16dbefe086e2a0ec9efe84cd92a032ba0109
alee1rivero <alee123rivero@gmail.com>
2023-02-22 17:41:48 -0300

    html padre

--- ProyectoEmpleados/carga_empleados/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CargaEmpleadosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'carga_empleados'
--- ProyectoEmpleados/carga_empleados/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from carga_empleados import views
-
-urlpatterns = [
-    path('', views.Inicio, name="inicio"),
-    path('empleados', views.empleados, name="empleados"),
-    path('empresa_terciarizada', views.empresa_terciarizada),
-    
-]
