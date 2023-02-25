08e7b2de6863d3a3b4217d8f4779939e4c4a9d96
alee1rivero <alee123rivero@gmail.com>
2023-02-25 11:32:35 -0300

    ya cree los formularios, Tengo problemas en el incio

--- carga_empleados/forms.py
+++ carga_empleados/forms.py
@@ -4,3 +4,14 @@ class EmpleadosFormularios(forms.Form):
     nombre=forms.CharField()
     apellido=forms.CharField()
     cargo=forms.CharField()
+
+class EmpresaFormularios(forms.Form):
+    nombre = forms.CharField()
+    razon_social = forms.CharField()
+    cuit = forms.IntegerField()
+
+class VehiculoFormularios(forms.Form):
+    marca=forms.CharField()
+    modelo=forms.CharField()
+    patente=forms.CharField()
+    
--- carga_empleados/views.py
+++ carga_empleados/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from carga_empleados.models import Empleado, empresa_terciarizada,vehiculo_empresarial
-from carga_empleados.forms import EmpleadosFormularios
+from carga_empleados.forms import EmpleadosFormularios,EmpresaFormularios,VehiculoFormularios
 # Create your views here.
 
 """
@@ -29,36 +29,31 @@ def empleados(request):
             miFormulario=EmpleadosFormularios()
     
         return render(request,"carga_empleados/empleados.html",{'miFormulario':miFormulario})
-    #return render(request, "carga_empleados/empleados.html")
-    #return HttpResponse("Vista empleados cargados al sistema")
 
 def empresa_terciarizada(request):
-    return render(request, "carga_empleados/empresa_terciarizada.html")
+        if request.method == 'POST':
+            miFormulario=EmpresaFormularios(request.POST)
+            print(miFormulario)
+
+            if miFormulario.is_valid:
+                informacion=miFormulario.cleaned_data
+                empleados = empresa_terciarizada(nombre=informacion["nombre"],razon_social=informacion['razon social'],cuit=informacion['cuit'])
+                empleados.save()
+                return render(request,'carga_empleados/inicio.html')
+        else:
+             miFormulario=EmpresaFormularios()
+        return render(request,'carga_empleados/empresa_terciarizada.html',{'miFormulario':miFormulario})
 
 def vehiculo_empresarial(request):
-    return render(request,"carga_empleados/vehiculo_empresarial.html")
-"""
-def empleadosFormularios(request):
-    if request.method == 'POST':
-        empleados = Empleado(request.POST["nombre"],(request.POST['apellido'],(request.POST['cargo'])))
-        empleados.save()
-        return render(render,'carga_empleados/inicio.html')
-    return render(request,"carga_empleados/empleadosFormularios.html")
+        if request.method == 'POST':
+            miFormulario=VehiculoFormularios(request.POST)
+            print(miFormulario)
 
-"""
-"""
-def empleadosFormularios(request):
-    if request.method == 'POST':
-        miFormulario= EmpleadosFormularios(request.POST)
-        print(miFormulario)
-
-        if miFormulario.is_valid:
-            informacion=miFormulario.cleaned_data
-            empleados = Empleado(nombre=informacion["nombre"],apellido=informacion['apellido'],cargo=informacion['cargo'])
-            empleados.save()
-            return render(request,'carga_empleados/inicio.html')
-    else:
-        miFormulario=EmpleadosFormularios()
-    
-    return render(request,"carga_empleados/empleados/empleadosFormularios.html",{'miFormulario':miFormulario})
-"""
+            if miFormulario.is_valid:
+                informacion=miFormulario.cleaned_data
+                empleados = vehiculo_empresarial( marca=informacion["Marca"],modelo=informacion['Modelo'],patente=informacion['Dominio'])
+                empleados.save()
+                return render(request,'carga_empleados/inicio.html')
+        else:
+             miFormulario=VehiculoFormularios()
+        return render(request,'carga_empleados/vehiculo_empresarial.html',{'miFormulario':miFormulario})
