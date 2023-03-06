d80780a9994b0a4f88319bd95523b84c861cb7b2
alee1rivero <alee123rivero@gmail.com>
2023-03-06 18:57:57 -0300

    creacion de login - register - logout

--- carga_empleados/urls.py
+++ carga_empleados/urls.py
@@ -1,14 +1,31 @@
 from django.urls import path
 from carga_empleados import views
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 urlpatterns = [
     path('', views.Inicio, name="inicio"),
     path('empleados', views.empleados,name="empleados"),
-    path('empresa_terciarizada', views.empresa_terciarizada,name="empresa_terciarizada"),
-    path('vehiculo_empresarial', views.vehiculo_empresarial,name="vehiculo_empresarial"),
+    path('empresa_terciarizada', views.Empresa_terciarizada,name="empresa_terciarizada"),
+    path('vehiculo_empresarial', views.Vehiculo_empresarial,name="vehiculo_empresarial"),
     #path('empleadosFormularios',views.empleadosFormularios,name="empleadosFormularios")
     path('leerEmpleados',views.leerempleados,name='LeerEmpleados'),
-    path('eliminarEmpleado/<empleado_nombre>',views.eliminarEmpleado, name= "eliminarEmpleado"),
-    path('editarEmpleado/<empleado_nombre>',views.editarEmpleado,name="editarEmpleado"),
+    path('eliminarEmpleado/<nombre_empleado>',views.eliminarEmpleado, name= "eliminarEmpleado"),
+    path('editarEmpleado/<nombre_empleado>',views.editarEmpleado,name="editarEmpleado"),
+    path('leerEmpresa',views.leerEmpresas,name='LeerEmpresa'),
+    path('eliminarEmpresa/<nombre_empresa>',views.eliminarEmpresa,name='eliminarEmpresa'),
+    path('editarEmpresa/<nombre_empresa>',views.editarEmpresa,name='editarEmpresa'),
+
+    path('empleado/list',views.EmpleadoList.as_view(),name='list'),
+    path(r'^(?P<pk>\d+)$',views.EmpleadoDetalle.as_view(),name='Detail'),
+    path(r'^nuevo$',views.EmpleadoCreacion.as_view(),name = 'New'),
+    path(r'^editar/(?P<pk>\d+)$',views.EmpleadoUpdate.as_view(),name='Edit'),
+    path(r'^borrar/(?P<pk>\d+)$',views.EmpleadoDelete.as_view(),name='Delete'),
+    path('login',views.login_request, name='Login'),
+    path('register',views.register,name='Register'),
+    path('logout', LogoutView.as_view(template_name='carga_empleados/logout.html'), name='Logout'),
+
+
+
 
 ]
--- carga_empleados/views.py
+++ carga_empleados/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from carga_empleados.models import Empleado, empresa_terciarizada,vehiculo_empresarial
-from carga_empleados.forms import EmpleadosFormularios,EmpresaFormularios,VehiculoFormularios
+from carga_empleados.forms import EmpleadosFormularios,EmpresaFormularios,VehiculoFormularios, UserRegisterForm
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout,authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-
+@login_required
 def Inicio(request):
     return render(request,"carga_empleados/inicio.html")
     #return HttpResponse("Vista inicio")
@@ -16,7 +24,7 @@ def empleados(request):
 
             if miFormulario.is_valid:
                 informacion=miFormulario.cleaned_data
-                empleados = Empleado(nombre=informacion["nombre"],apellido=informacion['apellido'],cargo=informacion['cargo'])
+                empleados = Empleado(nombre=informacion['nombre'],apellido=informacion['apellido'],cargo=informacion['cargo'])
                 empleados.save()
                 return render(request,'carga_empleados/inicio.html')
         else:
@@ -24,28 +32,28 @@ def empleados(request):
     
         return render(request,"carga_empleados/empleados.html",{'miFormulario':miFormulario})
 
-def empresa_terciarizada(request):
+def Empresa_terciarizada(request):
         if request.method == 'POST':
             miFormulario=EmpresaFormularios(request.POST)
             print(miFormulario)
 
             if miFormulario.is_valid:
                 informacion=miFormulario.cleaned_data
-                empresa = empresa_terciarizada(nombre=informacion["nombre"],razon_social=informacion['razon social'],cuit=informacion['cuit'])
+                empresa = empresa_terciarizada(nombre=informacion['nombre'],razon_social=informacion['razon_social'],cuit=informacion['cuit'])
                 empresa.save()
                 return render(request,'carga_empleados/inicio.html')
         else:
              miFormulario=EmpresaFormularios()
         return render(request,'carga_empleados/empresa_terciarizada.html',{'miFormulario':miFormulario})
 
-def vehiculo_empresarial(request):
+def Vehiculo_empresarial(request):
         if request.method == 'POST':
             miFormulario=VehiculoFormularios(request.POST)
             print(miFormulario)
 
             if miFormulario.is_valid:
                 informacion=miFormulario.cleaned_data
-                vehiculo = vehiculo_empresarial( marca=informacion["Marca"],modelo=informacion['Modelo'],patente=informacion['Dominio'])
+                vehiculo = vehiculo_empresarial(marca=informacion["marca"],modelo=informacion['modelo'],patente=informacion['patente'])
                 vehiculo.save()
                 return render(request,'carga_empleados/inicio.html')
         else:
@@ -82,4 +90,110 @@ def editarEmpleado(request,nombre_empleado):
             return render(request,'carga_empleados/inicio.html')
     else:
         miFormulario = EmpleadosFormularios(initial={'nombre':empleado.nombre,'apellido':empleado.apellido,'cargo':empleado.cargo})
-    return render(request,'carga_empleados/editarEmpleado.html',{'miFormulario':miFormulario,'empleado_nombre':empleado.nombre})
+    return render(request,'carga_empleados/editarEmpleados.html',{'miFormulario':miFormulario,'nombre_empleado':nombre_empleado})
+
+def leerEmpresas(request):
+    empresa = empresa_terciarizada.objects.all()
+    contexto = {'empresa':empresa}
+    return render(request,'carga_empleados/leerEmpresa.html', contexto)
+
+
+def eliminarEmpresa(request,nombre_empresa):
+    empresa= empresa_terciarizada.objects.get(nombre=nombre_empresa)
+    empresa.delete()
+
+    empresa = empresa_terciarizada.objects.all()
+    contexto = {'empresa': empresa}
+    return render(request,"carga_empleados/leerEmpresa.html",contexto)
+
+def editarEmpresa(request,nombre_empresa):
+    empresa = empresa_terciarizada.objects.get(nombre=nombre_empresa)
+    if request.method == 'POST':
+        miFormulario = EmpresaFormularios(request.POST)
+        print(miFormulario)
+
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            empresa.nombre = informacion['nombre']
+            empresa.razon_social = informacion['razon_social']
+            empresa.cuit = informacion['cuit']
+
+            empresa.save()
+
+            return render(request,'carga_empleados/inicio.html')
+    else:
+        miFormulario = EmpresaFormularios(initial={'nombre':empresa.nombre,'razon_social':empresa.razon_social,'cuit':empresa.cuit})
+    return render(request,'carga_empleados/editarEmpresa.html',{'miFormulario':miFormulario,'nombre_empresa':nombre_empresa})
+
+# Clases Basadas en Vistas de Empleados
+
+
+class EmpleadoList(ListView):
+     model=Empleado
+     template_name='carga_empleados/empleado_list.html'
+
+class EmpleadoDetalle(DetailView):
+     model= Empleado
+     template_name = 'carga_empleados/empleadoDetalle.html'
+
+class EmpleadoCreacion(CreateView):
+     model = Empleado
+     success_url = '/carga_empleados/empleado/list'
+     fields = ['nombre','apellido','cargo']
+    
+class EmpleadoUpdate(UpdateView):
+     model = Empleado
+     success_url = '/carga_empleados/empleado/list'
+     fields = ['nombre','apellido','cargo']
+    
+class EmpleadoDelete(DeleteView):
+     model = Empleado
+     success_url = '/carga_empleados/empleado/list'
+
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+
+        if form.is_valid():  
+            usuario = form.cleaned_data.get('username')
+            contra = form.cleaned_data.get('password')
+
+            user = authenticate(username= usuario, password=contra)
+
+            if user is not None:
+                login(request, user)
+
+                return render(request, "carga_empleados/inicio.html", {"mensaje":f"Bienvenido {usuario}"})
+            else:
+                return render(request, "carga_empleados/inicio.html", {"mensaje":"Datos incorrectos"})
+           
+        else:
+
+            return render(request, "carga_empleados/inicio.html", {"mensaje":"Formulario erroneo"})
+
+    form = AuthenticationForm()
+
+    return render(request, "carga_empleados/login.html", {"form": form})
+
+def register(request):
+
+      if request.method == 'POST':
+
+            #form = UserCreationForm(request.POST)
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+
+                  username = form.cleaned_data['username']
+                  form.save()
+                  return render(request,"carga_empleados/inicio.html" ,  {"mensaje":"Usuario Creado :)"})
+
+      else:
+            #form = UserCreationForm()       
+            form = UserRegisterForm()     
+
+      return render(request,"carga_empleados/registro.html" ,  {"form":form})
+
+
+
